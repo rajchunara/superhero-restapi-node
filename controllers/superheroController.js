d8c730b7bdfac3bfea5b0a41c6aebf53592244a1
rajchunara <rajchunara777@gmail.com>
2020-07-24 13:59:08 -0400

    added getting superhero by name

--- controllers/superheroController.js
+++ controllers/superheroController.js
@@ -1,6 +1,6 @@
-const superheroService = require("../services/superheroService");
+const superheroService = require('../services/superheroService');
 
-const superheroController = async (req, res, next) => {
+const superheroControllerWithId = async (req, res, next) => {
   const heroId = req.params.heroId;
   //   console.log(heroId);
   try {
@@ -15,6 +15,22 @@ const superheroController = async (req, res, next) => {
   }
 };
 
+const superheroControllerWithName = async (req, res, next) => {
+  const heroName = req.params.name;
+  //   console.log(heroId);
+  try {
+    const response = await superheroService.getSuperHeroByName(heroName);
+    res.status(200).json(response.res.data);
+    // console.log(response.res.data);
+    next();
+  } catch (e) {
+    res.status(404).json({
+      message: e.message,
+    });
+  }
+};
+
 module.exports = {
-  superheroController,
+  superheroControllerWithId,
+  superheroControllerWithName,
 };
--- routes/superhero.js
+++ routes/superhero.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const superhero = require("../controllers/superheroController");
+const express = require('express');
+const superhero = require('../controllers/superheroController');
 
 const superheroRouter = express.Router();
 
+//Endpoint app-url/superhero/id
 //Endpoint http://localhost:5000/superhero/id
-superheroRouter.get("/:heroId", superhero.superheroController);
+superheroRouter.get('/:heroId', superhero.superheroControllerWithId);
+superheroRouter.get('/search/:name', superhero.superheroControllerWithName);
 
 module.exports = superheroRouter;
